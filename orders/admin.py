546c34b3dc4810806ba26361394dde4b2807766f
halil <halil@DESKTOP-38P5TGT>
2024-12-01 23:48:40 +0300

    order model is created

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from orders.models import *
+
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
+admin.site.register(OrderResult)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,46 @@
+import uuid
 from django.db import models
+from products.models import Product  # ForeignKey için import
 
-# Create your models here.
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    company_name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_time = models.DateTimeField(null=True, blank=True)
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_time = models.DateTimeField(null=True, blank=True)
+
+class OrderDetail(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='details')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_time = models.DateTimeField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.count * self.unit_price
+        super().save(*args, **kwargs)
+
+class OrderResult(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='results')
+    result = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_time = models.DateTimeField(null=True, blank=True)
