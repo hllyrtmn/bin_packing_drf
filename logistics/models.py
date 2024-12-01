bd8ebadef3bf183c0f28f51cd646ec51f65236c2
halil <halil@DESKTOP-38P5TGT>
2024-12-02 01:11:44 +0300

    Switched database to PostgreSQL and re-applied migrations

--- logistics/models.py
+++ logistics/models.py
@@ -1,3 +1,33 @@
+import uuid
 from django.db import models
+from orders.models import Order  # ForeignKey için import
+from products.models import Product, Dimension  # Pallet ve paket boyutları için
 
-# Create your models here.
+class Pallet(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    dimension = models.ForeignKey(Dimension, on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_time = models.DateTimeField(null=True, blank=True)
+
+class Package(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='packages')
+    pallet = models.ForeignKey(Pallet, on_delete=models.SET_NULL, null=True, blank=True, related_name='packages')
+    rotation = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_time = models.DateTimeField(null=True, blank=True)
+
+class PackageDetail(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name='details')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_time = models.DateTimeField(null=True, blank=True)
