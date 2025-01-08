aed7fa6f299cba5fe0d7e025d435e8a872117e2a
halil <halil@DESKTOP-38P5TGT>
2025-01-09 02:40:30 +0300

    label created to pdf

--- logistics/services/excel_operation_services/settlement_service.py
+++ logistics/services/excel_operation_services/settlement_service.py
@@ -1,6 +1,11 @@
+from datetime import datetime
 from openpyxl import Workbook
 from openpyxl.styles import Font, Alignment, Border, Side
+from openpyxl.drawing.image import Image
 from openpyxl.utils import get_column_letter
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4, landscape
+from reportlab.lib.units import cm
 
 from logistics.models import PackageDetail
 
@@ -36,17 +41,23 @@ class SettlementService:
 
         row_num = 2  # Veri satırları 2. satırdan başlar
         col_widths = [len(col_name) for col_name in column_names]  # Sütun genişlikleri için başlık uzunluklarını başlangıç değeri olarak alıyoruz
-        
+        package_number = 0
         for package in packages:
+            package_number += 1
+            start_row = row_num
             package_details = PackageDetail.objects.filter(package_id=package.id)
+            aa = len(package_details)
+            aa = aa
             for package_detail in package_details:
                 horizontal_count = 0
                 if package.pallet.dimension.width % package_detail.product.dimension.width == 0 and package.pallet.dimension.depth % package_detail.product.dimension.depth == 0:
                     horizontal_count = package.pallet.dimension.width / package_detail.product.dimension.width * package.pallet.dimension.depth / package_detail.product.dimension.depth
                 elif package.pallet.dimension.width % package_detail.product.dimension.depth == 0 and package.pallet.dimension.depth % package_detail.product.dimension.width == 0:
                     horizontal_count = package.pallet.dimension.width / package_detail.product.dimension.depth * package.pallet.dimension.depth / package_detail.product.dimension.width
+                if horizontal_count == 0:
+                    horizontal_count = 1
                 values = [
-                    row_num - 1,  # "No"
+                    package_number,  # "No"
                     package.pallet.dimension.width/10 + 4,  # Palet Genişlik
                     "X",  # X
                     package.pallet.dimension.depth/10 + 2,  # Palet Derinlik
@@ -61,13 +72,13 @@ class SettlementService:
                     horizontal_count, #Yatay Sıra Adedi
                     package_detail.count / horizontal_count, # "Kat Adedi(Yatay)"
                     package_detail.count / horizontal_count, # "Eşdeğer",
-                    package_detail.count,  #"Ara Toplam",
-                    package_detail.count,  # Palet Adet
+                    package_detail.count,    #"Ara Toplam",
+                    sum(p_detail.count for p_detail in package_details),# Palet Adet
                     int(package_detail.count * package_detail.product.dimension.depth / 1000),  # Metre
                     round(float(package_detail.product.weight_type.std),2),  # Birim Ağırlık
                     round(float(package_detail.product.weight_type.std * package_detail.count),2),  # Net
                     round(float(package.pallet.weight),2),  # Palet Ağırlık
-                    round(float(package_detail.product.weight_type.std * package_detail.count + package.pallet.weight),2)  # Toplam
+                    round(float(sum(p_detail.product.weight_type.std * p_detail.count for p_detail in package_details) + package.pallet.weight),2)  # Toplam
                 ]
                 
                 # Satır verilerini hücrelere yaz
@@ -79,7 +90,12 @@ class SettlementService:
                     col_widths[col_num - 1] = max(col_widths[col_num - 1], len(str(value)))
 
                 row_num += 1  # Sonraki satır
-        
+            if row_num - start_row > 1:
+                merge_columns = [1, 2, 3, 4, 17, 21, 22]  # No, Palet Genişlik, X, Palet Derinlik sütunları
+                for col in merge_columns:
+                    ws.merge_cells(start_row=start_row, start_column=col, end_row=row_num - 1, end_column=col)
+                    merge_cell = ws.cell(row=start_row, column=col)
+                    merge_cell.alignment = Alignment(horizontal="center", vertical="center")
         # Toplam Satırı Ekle
         total_row = row_num
         total_label_cell = ws.cell(row=total_row, column=15, value="TOPLAM")
@@ -101,4 +117,48 @@ class SettlementService:
             ws.column_dimensions[get_column_letter(i + 1)].width = width + 2  # Ekstra boşluk için "+2"
 
         # Dosyayı kaydet
-        wb.save("uploads/report.xlsx")
+        wb.save(f"uploads/report_{datetime.now().date()}.xlsx")
+        SettlementService.create_pdf_label(packages)
+    @staticmethod
+    def create_pdf_label(packages):
+        # PDF oluşturma
+        c = canvas.Canvas("uploads/label_output.pdf", pagesize=A4)
+        width, height = A4
+        package_number = 1
+        for package in packages:
+            # Logo ekleme
+            logo_width, logo_height = 10 * cm, 3 * cm  # Logo boyutları
+            logo_x = (width - logo_width) / 2
+            logo_y = height - 5 * cm
+            c.drawImage("uploads/bedisa_white_bg.png", logo_x, logo_y, width=logo_width, height=logo_height)
+
+            # Dinamik "NO" bilgisi
+            c.setFont("Helvetica-Bold", 45)
+            c.drawCentredString(width / 2, height - 8 * cm, f"NO: {package_number}")
+            package_number +=1
+            # TYPE bilgisi
+            c.setFont("Helvetica-Bold", 16)
+            c.drawString(3 * cm, height - 11 * cm, "TYPE:")
+
+            # Ürün bilgileri
+            c.setFont("Helvetica", 18)
+            start_y = height - 13 * cm  # Ürün bilgileri başlangıç konumu
+            package_details = PackageDetail.objects.filter(package_id=package.id)
+            for package_detail in package_details:
+                c.drawString(4 * cm, start_y, f"{package_detail.product.product_type.type}.{package_detail.product.product_type.code}.{int(package_detail.product.dimension.width)}.{int(package_detail.product.dimension.depth)}")
+                c.drawString(14 * cm, start_y, f"Q: {sum(p_detail.count for p_detail in package_details)} PCS")
+                start_y -= 1 * cm  # Her ürün için satır kaydırma
+
+            # Şirket bilgisi
+            c.setFont("Helvetica-Bold", 20)
+            c.drawString(3 * cm, height - 20 * cm, f"COMPANY: {package.order.company.company_name}")
+
+            # Ülke bilgisi
+            c.setFont("Helvetica-Bold", 30)
+            c.drawCentredString(width / 2, height - 23 * cm, "RUSSIA")
+
+            # Yeni sayfa
+            c.showPage()
+
+        # PDF'i kaydet
+        c.save()
